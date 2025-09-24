a3b6ae14bf1be333612ae4eb4b049e2c41eeeae1
ajit kumar <54400990+ajitdehri@users.noreply.github.com>
2025-09-24 17:32:37 +0530

    latest modify

--- src/main/java/com/demo1/Demo1Application.java
+++ src/main/java/com/demo1/Demo1Application.java
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
 	public static void main(String[] args) {
+		String name="mike";
+		int x=100;
+		boolean ispresent=false;
+		int y=100;
+		char c='h';
+		int y=200;
+		double z=10.6;
+		boolean isPresent=false;
+		float m=10.56f;
 		SpringApplication.run(Demo1Application.class, args);
 	}
 
